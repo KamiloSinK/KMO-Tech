57486c027b7998fff3bfc7da64e2eefe6ef90278
Camilo Gómez <kamilo.sin.k@gmail.com>
2022-02-07 11:32:53 -0400

    Ver 3.0

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var app = new Vue ({
         newProd:'',
         newCant:'',
         newDesc:'',
-        listProd:'',
-        table: false
+        table: false,
+        likebar: 0
     },
 
     methods:{
@@ -18,9 +18,31 @@ var app = new Vue ({
                 cant: this.newCant,
                 desc: this.newDesc
             });
+            localStorage.setItem('productos', JSON.stringify(this.allProd));
             this.newProd='';
             this.newCant='';
             this.newDesc='';
+        },
+        delProd(index){
+            this.allProd.splice(index, 1);
+            localStorage.setItem('productos', JSON.stringify(this.allProd));
+        }
+    },
+    created(){
+        let datosDB = JSON.parse(localStorage.getItem('productos'));
+        if(datosDB === null){
+            this.allProd = [];
+        }else{
+            this.allProd = datosDB;
+        }
+    },
+
+    computed:{
+        barColor(){
+            return{
+                'red' : this.likebar < 50,
+                'green' : this.likebar >= 50   
+            }
         }
     }
 });
